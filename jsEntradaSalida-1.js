89ff21bf71f63a47649e5fa62ca1af7f7598cf61
MUJICA2468 <36281640+MUJICA2468@users.noreply.github.com>
2018-02-08 20:36:45 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,20 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{				
+
+
+
+var nombre;
+
+nombre = "Fabiàn";
+alert(nombre);
+var person = prompt("Heloo moto", "MOTO");
+
+}
+
+
+
+
+
+
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,28 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+
+
+//Defino una variable
+
+
+
+
+var nombre;
+//alert(nombre);
+nombre = prompt("Fabiàn","Gonzalez");
+alert(nombre);	 
+
+
+/*
+nombre  = "Fabiàn";
+alert(nombre);
+*/
+
+
+//var person = prompt("Harry", "Potter");
+
+	
+}
+
