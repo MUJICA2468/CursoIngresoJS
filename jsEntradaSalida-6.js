b21df7a823481920b6271588e6c986c241677ce0
MUJICA2468 <36281640+MUJICA2468@users.noreply.github.com>
2018-02-08 22:16:24 -0300

    Ejercicio 6
    document.g

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+
+
+	var numeroUno;
+	var numeroDos;
+
+	document.getElementById('numeroUno').value = numeroUno;	
+	document.getElementById('numeroDos').value = numeroDos;
+
+
+
+	numeroUno
+
+
+	numeroDos
+
+
+
+}
+
